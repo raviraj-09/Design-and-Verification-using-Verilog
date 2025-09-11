a22f85d6516048892cd95199f77a0139e5970596
Ravi Raj <108145514+raviraj-09@users.noreply.github.com>
2025-09-11 15:17:04 +0530

    Create comparator4bit.v

---
+++ 4 Bit Comparator/comparator4bit.v
@@ -0,0 +1,17 @@
+// Code your design here
+module comparator4bit(input [3:0] A,B,
+                      output reg G,L,E); // G: A>B; L: A<B; E: A=b;
+  
+  always@(*)
+    begin
+      G=0; L=0; E=0;
+      if(A>B)
+        G = 1'b1;
+      else if(A<B)
+        L = 1'b1;
+      else
+        E = 1'b1;
+    end
+endmodule
+  
+  
