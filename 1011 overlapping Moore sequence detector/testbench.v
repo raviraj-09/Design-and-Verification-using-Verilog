7cceda9d8112a3b79b76dd6f8b6ff415fa6c2997
Ravi Raj <108145514+raviraj-09@users.noreply.github.com>
2025-10-05 19:26:16 +0530

    Create testbench.v

---
+++ 1011 overlapping Moore sequence detector/testbench.v
@@ -0,0 +1,51 @@
+// Code your testbench here
+// or browse Examples
+
+// Code your testbench here
+// or browse Examples
+module seq_detector_1011_TB;
+  reg clk, rst_n, x;
+  wire z;
+  
+  seq_detector_1011 msd(clk, rst_n, x, z);
+  initial clk = 0;   
+  always #2 clk = ~clk;
+    
+  initial begin
+    x = 0;
+    #1 rst_n = 0;
+    #2 rst_n = 1;
+    
+    #3 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #10;
+    $finish;
+  end
+  
+  initial begin
+    // Dump waves
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
+endmodule
